3bfc30150a0d4c66c7607fcd908fae9311eace8f
AyushBodele <ayushbodele83@gmail.com>
2025-06-02 18:02:46 +0530

    Add n numbers

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const app = express();
+
+function sum(n){
+    let ans = 0
+    for (i = 0; i <= n; i++){
+        ans = ans + i
+    }
+    return ans;
+}
+
+app.get("/", function(req, res){
+    const n = req.query.n;
+    const ans = sum(n)
+    res.send("Hii There !" + ans)
+})
 app.listen(3000)
